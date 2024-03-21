8e8d9cbad3f429f7718eb0edd923f3b83d69a2ce
ali ahmed <aliahmed999416@gmail.com>
2024-03-21 18:00:48 +0200

    header file

---
+++ library/static_lib/include/myMath.h
@@ -0,0 +1,10 @@
+#ifndef MYMATH_H
+#define MYMATH_H
+
+int add( int x, int y);
+int sub(int x, int y);
+long int multiply(int x, int y);
+float divide(int x, int y);
+int modulus(int x, int y);
+
+#endif
