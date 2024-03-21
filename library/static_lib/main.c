609ba2c7f26b9cec020c98fc45b0843f8c96695d
ali ahmed <aliahmed999416@gmail.com>
2024-03-21 22:58:39 +0200

    document modification

--- library/static_lib/main.c
+++ library/static_lib/main.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#include"lib/myMath.h"
+#include"myMath.h"
 
 
 
@@ -11,7 +11,7 @@ int main()
 	
 	printf("Addition Result: %d \n", add(x, y));
 	printf("Subtraction Result: %d \n", sub(x, y));
-	printf("Multiplication Result: %d \n", multiply(x, y));
+	printf("Multiplication Result: %ld \n", multiply(x, y));
 	printf("Division Result: %f \n", divide(x, y));
 	printf("Modulus Result: %d \n", modulus(x, y));
 }
